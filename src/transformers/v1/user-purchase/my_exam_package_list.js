a7ed982e508ee9405a6271d9202b445b4b20b9c0
nawa316 <muhammadadedzakwan@gmail.com>
2025-08-06 11:53:10 +0700

    fix: item

--- src/transformers/v1/user-purchase/my_exam_package_list.js
+++ src/transformers/v1/user-purchase/my_exam_package_list.js
@@ -1,10 +1,12 @@
 exports.item = (data) => ({
-    uuid: data.uuid,
-    title: data.title,
-    category: data.categories.map((category) => ({
-        uuid: category.uuid,
-        title: category.title
-    }))
+  uuid: data.uuid || "",
+  title: data.title || "",
+  category: Array.isArray(data.categories)
+    ? data.categories.map((category) => ({
+        uuid: category?.uuid || "",
+        title: category?.title || "",
+      }))
+    : [],
 });
 
 module.exports = exports;
