0c75b56ba73e43eea39259cb790fae37235a3286
maroqijalil <jalilpro31@gmail.com>
2025-08-17 23:10:45 +0700

    refactor: review prompt

--- src/utils/essay_review.js
+++ src/utils/essay_review.js
@@ -1,7 +1,5 @@
 const CommonConstants = require('../constants/common');
 
-const SCHOLARSHIP = 'LPDP';
-
 const uniqInputEssayItems = (essayItems) => {
     const essayItemUuids = [];
     const newEssayItems = [];
@@ -18,8 +16,8 @@ const uniqInputEssayItems = (essayItems) => {
 const getEssayReviewUserPrompt = (criteria, content) => `KRITERIA: ${criteria}\n\nESSAI ASLI DARI CANDIDATE: ${content}`;
 
 const getEssayReviewSystemPrompt = (backgroundDescription, topic, language = CommonConstants.LANGUAGE.ENGLISH) => `
-Anda adalah reviewer esai beasiswa ${SCHOLARSHIP} yang berpengalaman 25 tahun sebagai praktisi dan akademisi di bidang sesuai yang disebut di essay di bawah. 
-Anda menilai sebuah esai beasiswa ${SCHOLARSHIP} terkait ${topic} dari kandidat dengan latar belakang berikut:
+Anda adalah reviewer esai beasiswa yang berpengalaman 25 tahun sebagai praktisi dan akademisi di bidang sesuai yang disebut di essay di bawah. 
+Anda menilai sebuah esai beasiswa terkait ${topic} dari kandidat dengan latar belakang berikut:
 "${backgroundDescription}"
 
 Lalu, kamu wajib memperbaiki/meningkatkan esai yang dia kirimkan agar semakin sesuai dan kuat, jadi jangan hanya sekadar memberikan komentar, tetapi juga memberikan solusi!.
