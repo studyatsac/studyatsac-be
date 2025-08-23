aaaf2e9096d8820207270051dc28183eb3dbd150
nawa316 <muhammadadedzakwan@gmail.com>
2025-08-23 23:49:45 +0700

    fix: update resource

--- src/methods/v1/resources/admin_update.js
+++ src/methods/v1/resources/admin_update.js
@@ -3,7 +3,6 @@ const ResourcesRepository = require('../../../repositories/mysql/resources');
 exports.updateResource = async (req, res) => {
     try {
         const id = req.params.id;
-        const updateData = req.body;
         if (!id) {
             return res.status(400).json({ message: 'Resource id is required' });
         }
@@ -12,6 +11,9 @@ exports.updateResource = async (req, res) => {
         if (!resource) {
             return res.status(404).json({ message: 'Resource not found' });
         }
+
+        const { resourceName, type, sourceLink } = req.body;
+        const updateData = { resource_name: resourceName, type, source_link: sourceLink };
         await ResourcesRepository.update({ id }, updateData);
         const updatedResource = await ResourcesRepository.findOne({ id });
         return res.status(200).json({ data: updatedResource });
