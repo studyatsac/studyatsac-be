49f6772502d4bb89c664608c912445339aafe199
nawa316 <muhammadadedzakwan@gmail.com>
2025-09-23 14:29:00 +0700

    feat: add change language functionality

--- src/middlewares/token_middleware.js
+++ src/middlewares/token_middleware.js
@@ -2,12 +2,21 @@
 
 const AuthService = require('../services/v1/account');
 
+const SUPPORTED_LANGUAGES = ['id', 'en'];
+const DEFAULT_LANGUAGE = 'id';
+
 module.exports = async (req, res, next) => {
     try {
         const headers = req.headers || {};
         const token = headers.authorization || headers.Authorization || '';
         const sanitizedToken = token.trim().split(' ').pop();
         let message = 'Authentication token not valid';
+
+        const langHeader = req.headers['accept-language'] || '';
+
+        const preferredLang = langHeader.split(',')[0].split('-')[0].toLowerCase();
+
+        req.locale = SUPPORTED_LANGUAGES.includes(preferredLang) ? preferredLang : DEFAULT_LANGUAGE;
 
         if (!sanitizedToken) {
             return res.status(401).json({ message });
